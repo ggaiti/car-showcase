a6de026fea7b42a39e5c9cbabbfdcc8ffa746a0e
ggaiti <ggaiti@hotmail.com>
2024-01-08 22:19:34 -0500

    added images, type interface.

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+"use client";
 
-const CustomButton = () => {
-  return <div>Custom Button</div>;
+import { CustomButtonProps } from "@/types";
+import Image from "next/image";
+
+const CustomButton = ({
+  title,
+  containerStyles,
+  handleClick,
+}: CustomButtonProps) => {
+  return (
+    <button
+      disabled={false}
+      type={"button"}
+      className={`custom-btn ${containerStyles}`}
+      onClick={handleClick}
+    >
+      <span className={`flex-1`}>{title}</span>
+    </button>
+  );
 };
 
 export default CustomButton;
