7059d6a3517789b678b86445cc4274d120799bbd
ggaiti <ggaiti@hotmail.com>
2024-02-07 03:01:40 -0500

    created CarDetails, added Image in carCard

--- utils/index.ts
+++ utils/index.ts
@@ -1,25 +1,28 @@
-export async function fetchCars(){
-    const headers = {
-    'X-RapidAPI-Key': 'ff2d2e1a48msh74812854e021f12p1e1fd9jsn8e1e68aa082b',
-    'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+export async function fetchCars() {
+  const headers = {
+    "X-RapidAPI-Key": "ff2d2e1a48msh74812854e021f12p1e1fd9jsn8e1e68aa082b",
+    "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+  };
+  const resposnse = await fetch(
+    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla",
+    {
+      headers: headers,
     }
-    const resposnse =  await fetch("https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla",{
-        headers: headers,
-    })
-    const result = await resposnse.json();
-    return result
+  );
+  const result = await resposnse.json();
+  return result;
 }
 export const calculateCarRent = (city_mpg: number, year: number) => {
-    const basePricePerDay = 50; // Base rental price per day in dollars
-    const mileageFactor = 0.1; // Additional rate per mile driven
-    const ageFactor = 0.05; // Additional rate per year of vehicle age
-  
-    // Calculate additional rate based on mileage and age
-    const mileageRate = city_mpg * mileageFactor;
-    const ageRate = (new Date().getFullYear() - year) * ageFactor;
-  
-    // Calculate total rental rate per day
-    const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
-  
-    return rentalRatePerDay.toFixed(0);
-  };
+  const basePricePerDay = 50; // Base rental price per day in dollars
+  const mileageFactor = 0.1; // Additional rate per mile driven
+  const ageFactor = 0.05; // Additional rate per year of vehicle age
+
+  // Calculate additional rate based on mileage and age
+  const mileageRate = city_mpg * mileageFactor;
+  const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+  // Calculate total rental rate per day
+  const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+  return rentalRatePerDay.toFixed(0);
+};
